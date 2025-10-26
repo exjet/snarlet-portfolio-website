f8bf35e48bd5f3908cee03404837c89df5b16967
ExJet <executorj@gmail.com>
2025-10-26 19:38:11 -0400

    More beauty

--- assets/js/app.js
+++ assets/js/app.js
@@ -228,7 +228,7 @@ let footer = $(`
 
 
 
-    <a class="social-button instagram" href="" target="_blank" >
+    <a class="social-button instagram" href="https://www.instagram.com/snarloid_prime/" target="_blank" >
     <!-- svg code for instagram icon -->
       <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
         style="enable-background:new 0 0 202.5 202.5;">
